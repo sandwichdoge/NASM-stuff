a14f3a436dc8468b44490b15e08da144a7bde5ee
sandwichdoge <38368906+sandwichdoge@users.noreply.github.com>
2018-08-06 15:43:38 +0700

    read a value from stdin then print it to stdout

--- scan-val.asm
+++ scan-val.asm
@@ -7,15 +7,15 @@ _start:
                 ;read from stdin
                 xor rax, rax
                 xor rdi, rdi
-                lea rsi, [rsp - 8]  ;write on top of stack
-                mov rdx, 12
+                lea rsi, [rsp - 64]  ;write on top of stack, 64bytes buffer
+                mov rdx, 64
                 syscall
 
                 ;print to stdout
                 mov rax, 1 ;write
                 mov rdi, 1 ;stdout
-                lea rsi, [rsp -8] ;read from top of stack
-                mov rdx, 12
+                lea rsi, [rsp - 64] ;read from top of stack
+                mov rdx, 64
                 syscall
 
                 call _exit
